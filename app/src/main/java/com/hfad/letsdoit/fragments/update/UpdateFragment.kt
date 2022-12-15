50b29e153f6414449aadaa1579ef31e21a72ffc4
Najimkhon <qwerty1995.290@gmail.com>
2022-12-15 23:40:34 +0500

    packages created

--- app/src/main/java/com/hfad/letsdoit/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/hfad/letsdoit/fragments/update/UpdateFragment.kt
@@ -1,19 +1,19 @@
-package com.hfad.letsdoit
+package com.hfad.letsdoit.fragments.update
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.hfad.letsdoit.R
 
-class AddFragment : Fragment() {
-
+class UpdateFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        return inflater.inflate(R.layout.fragment_update, container, false)
     }
 
 }
