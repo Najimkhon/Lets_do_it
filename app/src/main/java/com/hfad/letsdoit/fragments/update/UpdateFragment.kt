ffdd9c4cc608967b66cf871209b5345da2c55cf1
Najimkhon <qwerty1995.290@gmail.com>
2022-12-16 14:22:17 +0500

    UpdateFragment Menu id added

--- app/src/main/java/com/hfad/letsdoit/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/hfad/letsdoit/fragments/update/UpdateFragment.kt
@@ -1,10 +1,8 @@
 package com.hfad.letsdoit.fragments.update
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.hfad.letsdoit.R
 
 class UpdateFragment : Fragment() {
@@ -13,7 +11,12 @@ class UpdateFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_update, container, false)
+
+    }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.update_fragment_menu, menu)
     }
 
 }
