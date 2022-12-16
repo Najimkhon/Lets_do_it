4e0d8f5fdc4894b4a290a0c3265af97f30c54e76
Najimkhon <qwerty1995.290@gmail.com>
2022-12-16 22:42:15 +0500

    created ViewModel

---
+++ app/src/main/java/com/hfad/letsdoit/data/viewmodel/ToDoViewModel.kt
@@ -0,0 +1,32 @@
+package com.hfad.letsdoit.data.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.hfad.letsdoit.data.ToDoDatabase
+import com.hfad.letsdoit.data.models.ToDoData
+import com.hfad.letsdoit.data.repository.ToDoRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class ToDoViewModel(application:Application):AndroidViewModel(application) {
+
+    private val toDoDAO = ToDoDatabase.getDatabase(
+        application
+    ).toDoDao()
+    private val repository: ToDoRepository
+    private val getAllData: LiveData<List<ToDoData>>
+
+
+    init {
+        repository = ToDoRepository(toDoDAO)
+        getAllData = repository.getAllData
+    }
+
+    fun insertData(toDoData: ToDoData){
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.insertData(toDoData)
+        }
+    }
+}
