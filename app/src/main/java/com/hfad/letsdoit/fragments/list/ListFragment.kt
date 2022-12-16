ca3765dda994430ca0f2f9485a9c59368ac8ec20
Najimkhon <qwerty1995.290@gmail.com>
2022-12-16 12:01:03 +0500

    listFragment menu is designed

--- app/src/main/java/com/hfad/letsdoit/fragments/list/ListFragment.kt
+++ app/src/main/java/com/hfad/letsdoit/fragments/list/ListFragment.kt
@@ -1,10 +1,8 @@
 package com.hfad.letsdoit.fragments.list
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.hfad.letsdoit.R
 import com.hfad.letsdoit.databinding.FragmentListBinding
@@ -26,9 +24,16 @@ class ListFragment : Fragment() {
         binding.listLayout.setOnClickListener{
             findNavController().navigate(R.id.action_listFragment_to_updateFragment)
         }
+        setHasOptionsMenu(true)
 
         return view
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu, menu)
+    }
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
